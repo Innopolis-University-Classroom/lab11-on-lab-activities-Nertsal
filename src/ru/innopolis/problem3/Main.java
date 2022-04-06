e9968ff9fe675348b93b1281b6d65d4a67d37586
Nertsal <sasha.kudasov04@gmail.com>
2022-04-06 17:07:54 +0300

    Problem 3

--- src/ru/innopolis/problem3/Main.java
+++ src/ru/innopolis/problem3/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+    }
+}
+
+interface MediaPlayer {
+    void playMp4();
+}
+
+interface AudioPlayer {
+    void playMp3();
+}
+
+class MediaAdapter implements MediaPlayer {
+    private Audio audio;
+
+    public MediaAdapter(Audio audio) {
+        this.audio = audio;
+    }
+
+    public void playMp4() {
+        audio.playMp3();
+    }
+}
+
+class Media implements MediaPlayer {
+    public void playMp4() {
+    }
+}
+
+class Audio implements AudioPlayer {
+    public void playMp3() {
+    }
+}
+
+class PlayerDevice {
+    void playMp4(MediaPlayer player) {
     }
 }
