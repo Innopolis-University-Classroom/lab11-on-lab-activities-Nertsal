1ce311896171860579e64acfe6ba82ee319eb6d6
Nertsal <sasha.kudasov04@gmail.com>
2022-04-06 16:47:31 +0300

    Problem 1

--- src/ru/innopolis/problem1/problem1.java
+++ src/ru/innopolis/problem1/problem1.java
@@ -1,2 +1,31 @@
-// your solution here
-//fix all the errors including package errors
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Workshop carWorkshop = new Workshop();
+
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+        if (next.toUpperCase().equals("A")) {
+            carWorkshop.setWorkshop(message -> System.out.println("we Should assembles it"));
+        } else if (next.toUpperCase().equals("D")) {
+            carWorkshop.setWorkshop(message -> System.out.println("we Should disassembles it"));
+        }
+    }
+}
+
+interface Operation {
+    void operate(String input);
+}
+
+class Workshop {
+    private Operation workshop;
+
+    public void startFiltering(String input) {
+        workshop.operate(input);
+    }
+
+    public void setWorkshop(Operation workshop) {
+        this.workshop = workshop;
+    }
+}
